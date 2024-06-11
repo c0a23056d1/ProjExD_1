f2baf2fea3e28cb0dce124cd7a0a0aa31b76016a
C0A23056 <c0a23056d1@edu.teu.ac.jp>
2024-06-11 16:36:30 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #背景画像
+    bg_img2 = pg.transform.flip(bg_img, True, False) #背景画像
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -17,8 +18,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0]) #背景画像を表すsurfase
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0]) #背景画像を表すsurfase
+        screen.blit(bg_img2, [-x+4800, 0])
         kk_rect = kk_img.get_rect() #こうかとんrectの抽出
         kk_rect.center = 300, 200
         screen.blit(kk_img, kk_rect) #kk_imageをkk_rectの設定に従って貼り付け
